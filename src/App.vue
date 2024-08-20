85b951053f0d22903cbfa2a5c64836d19de51f55
Kim Eirik Kvassheim <kim.eirik.kvassheim@uniguest.com>
2024-08-20 17:14:23 +0200

    Playing with example data

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,108 @@
+<template>
+
+
+    <div v-if="showTodoList">
+        <h1>{{ currentTodoList.name }}</h1>
+        <p>{{ currentTodoList.description }}</p>
+        <p>Config: <pre>{{ currentTodoList.config }}</pre></p>
+
+        <br><br>
+
+        <table>
+            <tr>
+                <th>Name</th>
+                <th>Qty</th>
+                <th>Done</th>
+            </tr>
+            <tr v-for="entry in currentTodoEntries" :key="entry.id">
+                <td>{{ entry.name }}</td>
+                <td>{{ entry.qty }}</td>
+                <td>{{ entry.done }}</td>
+            </tr>
+        </table>
+
+        <br><br>
+
+        <button type="button" @click="showTodoList = false">&raquo; Back to my Todo lists</button>
+
+    </div>
+
+
+    <div v-else>
+        <h1>My Todo lists</h1>
+        <table>
+            <tr>
+                <th>ID</th>
+                <th>Created</th>
+                <th>Name</th>
+                <th>Description</th>
+                <th>Config</th>
+                <th>Owner</th>
+            </tr>
+            <tr v-for="todoList in myTodoLists" :key="todoList.id" @click="openTodoList(todoList)">
+                <td>{{ todoList.id }}</td>
+                <td>{{ todoList.created_at }}</td>
+                <td>{{ todoList.name }}</td>
+                <td>{{ todoList.description }}</td>
+                <td>{{ todoList.config }}</td>
+                <td>{{ todoList.owner }}</td>
+            </tr>
+        </table>
+    </div>
+
+</template>
+
+<style>
+table th {
+    font-weight:bold;
+    border-bottom:1px solid #333;
+}
+table td {
+    padding:10px;
+    border-bottom:1px solid dodgerblue;
+    cursor:pointer;
+}
+table tr:hover {
+    background:rgba(0,0,0,0.02);
+}
+button {
+    padding:10px;
+    font-weight:bold;
+    cursor:pointer;
+}
+</style>
+
 <script setup>
 import { ref, onMounted } from 'vue'
 import { supabase } from './lib/supabaseClient'
-// require('dotenv').config()
 
 const groceries = ref([])
+const myTodoLists = ref([]);
+let showTodoList = ref(false)
+let currentTodoList = ref();
+let currentTodoEntries = ref([]);
 
-async function getGroceries() {
-    const { data } = await supabase.from('groceries').select()
-    groceries.value = data
+async function fetchTodoLists() {
+    const { data } = await supabase.from('todolists').select();
+    myTodoLists.value = data;
 }
 
-onMounted(() => {
-    getGroceries()
-})
-</script>
+async function fetchEntriesInTodolist(todoListId) {
+    const { data } = await supabase.from('todoentries').select().eq('todolist_id', todoListId);
+    currentTodoEntries.value = data;
+}
 
-<template>
-    <ul>
-        <li v-for="grocery in groceries" :key="grocery.id">{{ grocery.name }}</li>
-    </ul>
-</template>
+async function openTodoList(todoList) {
+    console.log('openTodoList', todoList.id)
+    this.showTodoList = true;
+    currentTodoList = todoList;
+    await fetchEntriesInTodolist(todoList.id);
+}
+
+
+
+onMounted(() => {
+    fetchTodoLists();
+});
+
+</script>
